f58f011ba32a93ac269ddd2f45e4507135830ac4
Fabio Brasileiro <fabiotrocados@debian-BULLSEYE-live-builder-AMD64>
2024-06-19 22:57:38 -0400

    Update Button component with new styles and click effects

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,5 +1,4 @@
 import { ComponentProps } from 'react'
-
 import { cn } from '@/utils/merge-classes'
 
 interface ButtonProps extends ComponentProps<'button'> {}
@@ -8,7 +7,9 @@ export function Button({ className, ...props }: ButtonProps) {
   return (
     <button
       className={cn(
-        'rounded-full border-2 border-sunset-50 bg-sunset-400 px-8 py-4 text-center text-lg font-bold text-sunset-950 shadow-[6px_6px_0_0_rgba(255,255,255,1)]',
+        'relative block mx-auto bg-sunset-400 text-lg font-bold text-sunset-950 px-8 py-4 rounded-full shadow-[0_9px_#efa424] outline-none border-none cursor-pointer transition-all duration-150',
+        'hover:shadow-[0_4px_#efa424] hover:top-[2px]',
+        'active:shadow-none active:top-[6px] active:bg-sunset-500',
         className,
       )}
       {...props}
